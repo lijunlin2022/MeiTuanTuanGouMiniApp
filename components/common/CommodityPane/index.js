94127ded5e60791cd19a0240754ac36d738a0543
a-layfolk <1808150553@qq.com>
2022-02-20 17:29:46 +0800

    feat: package component CommodityPane

---
+++ components/common/CommodityPane/index.js
@@ -0,0 +1,25 @@
+Component({
+  properties: {
+    tag: {
+      type: String
+    },
+    iconPath: {
+      type: String
+    },
+    address: {
+      type: String
+    },
+    sales: {
+      type: String
+    },
+    time: {
+      type: String
+    },
+    discount: {
+      type: String
+    },
+    action: {
+      type: String
+    }
+  }
+})
